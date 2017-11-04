9c2a3f621a2031d2ebf6a49f6a414cd38ad87418
C <fortraan1221@gmail.com>
2017-11-03 23:00:48 -0500

    Preparing for pull

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competition/KI4.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes.competition;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by christopher.johnson on 10/30/17.
+ */
+
+public class KI4 extends OpMode {
+}
---
+++ openCVTeamCode/src/main/java/com/smartstar/opencvteamcode/opmodes/JewelAutoTestMode.java
@@ -0,0 +1,10 @@
+package com.smartstar.opencvteamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by christopher.johnson on 10/30/17.
+ */
+
+public class JewelAutoTestMode extends LinearOpMode {
+}
